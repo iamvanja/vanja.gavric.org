4ed53f015c64faa22e9be26e1a3b8a0633edb3ee
iamvanja <vanja@gavric.org>
2013-11-04 00:52:10 +0100

    added /min script

---
+++ min/groupsConfig.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+return array(
+    'main.min.js' => array(
+        '//assets/js/vendor/jquery.easing.1.3.js', 
+        '//assets/js/vendor/jquery.backstretch.js', 
+        '//assets/js/vendor/skrollr.js', 
+        '//assets/js/main.js'
+    )
+);
