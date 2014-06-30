2929820e354765026a963a19615f1dcaca2093af
iamvanja <vanja@gavric.org>
2014-06-29 17:06:37 -0700

    Added new Universal Google Analytics snippet

--- v1/index.php
+++ v1/index.php
@@ -44,17 +44,15 @@
 <!--[if lte IE 7]> <style type="text/css">#ie6-error {display:block;}</style><![endif]-->	
 <!--[if IE 8]> <style type="text/css">#ie8-error {display:block;}</style><![endif]-->	
 <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]--> 
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-24663272-1']);
-  _gaq.push(['_trackPageview']);
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+        ga('create', 'UA-24663272-1', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 
 <body>
