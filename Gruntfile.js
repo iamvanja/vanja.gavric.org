b0016c3780012e6f57deae94965bb337d8c33553
iamvanja <vanja@gavric.org>
2016-09-05 22:23:41 -0700

    Added production build step

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,11 @@
 module.exports = function(grunt){
     require("jit-grunt")(grunt, {
-        pug: "grunt-pug-i18n"
+        pug: "grunt-pug-i18n",
+        removelogging: "grunt-remove-logging"
     });
 
     var settings = grunt.file.readJSON("settings.json"),
+        shell = require("shelljs"),
         helpers = {
             pugData: function(options) {
                 options = options || {};
@@ -30,7 +32,20 @@ module.exports = function(grunt){
 
                 // console.log("done", preparedData, data);
                 return preparedData;
-            }
+            },
+            getExec: function(command) {
+                return ""+((shell.exec(command).output).replace("\n", "")).replace(/\\/g, "/");
+            },
+            getProductionFiles: function(filesArr){
+                var prodJsFiles = [];
+
+                for (var i = 0, len = filesArr.length; i < len; i++) {
+                    // replaces only first occurence, perfect for our use-case
+                    prodJsFiles.push( filesArr[i].replace("src/", "build/") );
+                }
+
+                return prodJsFiles;
+            },
         };
 
     grunt.initConfig({
@@ -85,6 +100,26 @@ module.exports = function(grunt){
                 }]
             }
         },
+        htmlhint: {
+            watch: {
+                options: {
+                    "tag-pair": true,
+                    "tagname-lowercase": true,
+                    "attr-lowercase": true,
+                    "attr-value-double-quotes": true,
+                    "doctype-first": true,
+                    "spec-char-escape": true,
+                    "id-unique": true
+                },
+                src: settings.htmlWatchFiles
+            },
+            production: {
+                options: "<%= htmlhint.watch.options %>",
+                src: helpers.getProductionFiles(settings.htmlWatchFiles)
+            }
+        },
+
+
 
         // JS
         concat: {
@@ -94,6 +129,35 @@ module.exports = function(grunt){
             watch: {
                 dest: "src/assets/js/main.min.js",
                 src: settings.jsFiles
+            }
+        },
+        removelogging: {
+            production: {
+                src: [
+                    "build/assets/js/*.js"
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                sourceMapIncludeSources: true
+            },
+            production: {
+                files: [
+                    {
+                        dest: "build/assets/js/main.min.js",
+                        src: helpers.getProductionFiles(settings.jsFiles)
+                    },
+                    {
+                        expand: true,
+                        cwd: "build/",
+                        src: [
+                            "assets/js/init-*.js",
+                        ],
+                        dest: "build/"
+                    }
+                ]
             }
         },
 
@@ -122,7 +186,6 @@ module.exports = function(grunt){
                 }
             }
         },
-
         postcss: {
             options: {
                 processors: [
@@ -139,6 +202,68 @@ module.exports = function(grunt){
             },
             production: {
                 src: "build/assets/css/main.css"
+            }
+        },
+
+        // IMAGES
+        imagemin: {
+            options: {
+                cache: false
+            },
+
+            production: {
+                files:[
+                    {
+                        expand: true,
+                        cwd: "build/assets/images/",
+                        src: [
+                            "**/*.{png,jpg,gif}"
+                        ],
+                        dest: "build/assets/images/"
+                    },
+                    {
+                        expand: true,
+                        cwd: "build/v1/images/",
+                        src: [
+                            "**/*.{png,jpg,gif}"
+                        ],
+                        dest: "build/v1/images/"
+                    },
+                ]
+            }
+        },
+
+        // VARIOUS
+        clean: {
+            productionPre: [
+                "build"
+            ],
+            productionPost: [
+                "build/assets/css/scss/**",
+                "build/assets/js/foundation.min.js",
+                "build/assets/js/main.js",
+                "build/assets/pug-*/**"
+            ]
+        },
+        copy: {
+            production: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/",
+                        src: [
+                            "**",
+                            ".htaccess",
+                            "!old/**",
+                            "!assets/js/foundation/**",
+                            "!**/*.psd",
+                            "!assets/css/main.css",
+                            // "!_src/**",
+                            // "!assets/_psd_other/**",
+                        ],
+                        dest: "build/"
+                    }
+                ]
             }
         },
 
@@ -164,14 +289,14 @@ module.exports = function(grunt){
                     livereload: true
                 }
             },
-            // html: {
-            //     files: "<%= htmlWatchFiles.watch %>",
-            //     tasks: ["htmlhint:watch"],
-            //     options: {
-            //         // Start a live reload server on the default port 35729
-            //         livereload: true
-            //     }
-            // },
+            html: {
+                files: settings.htmlWatchFiles,
+                tasks: ["htmlhint:watch"],
+                options: {
+                    // Start a live reload server on the default port 35729
+                    livereload: true
+                }
+            },
             css: {
                 files: ["src/assets/css/scss/**/*.scss"],
                 tasks: ["compass:watch", "postcss:watch"],
@@ -192,4 +317,36 @@ module.exports = function(grunt){
             }
         }
     });
+
+    grunt.registerTask("setEnvironmentVars", "set environment variables", function() {
+        var getExec = grunt.config.data.helpers.getExec,
+            gitBranch = getExec("git rev-parse --abbrev-ref HEAD"),
+            gitHash = getExec("git log -n 1 --pretty=format:\"%H\""),
+            currentUser = getExec("git config user.name");
+
+        if (gitBranch.trim() === "" || shell.exec("curl").code === 127) {
+            grunt.log.errorlns("Please make sure `git` and `curl` are installed and in your $PATH.");
+            return grunt.fail.fatal("Pushing without necessary tools is not allowed!");
+        }
+        grunt.config.set("environment", gitBranch === "master" ? "production" : "test-production ("+ gitBranch +")");
+        grunt.config.set("gitBranch", gitBranch);
+        grunt.config.set("gitHash", gitHash);
+        grunt.config.set("currentUser", currentUser);
+    });
+
+    grunt.registerTask("default", []);
+    grunt.registerTask("production", [
+        "setEnvironmentVars",
+        // "jshint",
+        "clean:productionPre",
+        "copy:production",
+        "imagemin:production",
+        "compass:production",
+        "postcss:production",
+        "removelogging:production",
+        "uglify:production",
+        "pug:production",
+        "clean:productionPost",
+        "htmlhint:production"
+    ]);
 }
