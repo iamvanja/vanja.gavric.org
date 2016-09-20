7443835314f569e3d030109c63b582fa75183bdc
iamvanja <vanja@gavric.org>
2016-09-19 19:05:51 -0700

    Copy jQuery on production build

--- grunt/configs/jquery.js
+++ grunt/configs/jquery.js
@@ -1,15 +1,36 @@
 "use strict";
 
-module.exports = {
-    dev: {
-      options: {
-        prefix: "jquery-",
-        minify: false
-      },
-      output: "src/assets/js/vendor",
-      versions: {
-        // Remove everything we don't need from 2.x versions
-        "2.1.1": [ "ajax", "deprecated", "wrap", ],
-      }
-    }
+module.exports = function(grunt, data) {
+    console.log("hahaha", data);
+    var defaults = {
+            options: {
+                prefix: "jquery-",
+                minify: false
+            },
+            output: "/assets/js/vendor",
+            versions: {
+                // Remove everything we don't need from 2.x versions
+                "2.1.1": [ "ajax", "deprecated", "wrap", ],
+            }
+        },
+        config = {
+            dev: {
+                options: {
+                    prefix: defaults.options.prefix,
+                    minify: false,
+                },
+                output: "src" + defaults.output,
+                versions: defaults.versions
+            },
+            production: {
+                options: {
+                    prefix: defaults.options.prefix,
+                    minify: true,
+                },
+                output: "build" + defaults.output,
+                versions: defaults.versions
+            }
+        };
+
+    return config;
 };
