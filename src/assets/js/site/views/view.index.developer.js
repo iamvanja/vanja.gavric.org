7147c7327d473938c104fb56de1813458793274b
iamvanja <vanja@gavric.org>
2016-09-28 21:46:13 -0700

    Added an alternative way to select skills on mobile, external data for technologies/skills

--- src/assets/js/site/views/view.index.developer.js
+++ src/assets/js/site/views/view.index.developer.js
@@ -6,47 +6,77 @@
             el: "#developer",
             list: "#technologies-list",
             legend: "#technologies-legend",
+            select: "#technologies-select",
+            technologiesWrap: "#developer .technologies-wrap",
         },
+        cachedUi = {},
         activeCategoryName,
         shuffleTechnologies = function(){
-            site.helpers.shuffleList($(ui.list)[0] || []);
+            site.helpers.shuffleList(cachedUi.$list[0] || []);
         },
         initTechnologiesLegend = function(){
-            $(ui.legend).find("span").on({
+            cachedUi.$legend.find("span").on({
                 mouseenter: function(){
-                    filterTechnologies(this);
+                    activeCategoryName = $(this).attr("class");
+                    filterTechnologies(activeCategoryName);
                 },
                 mouseleave: function(){
                     removeTechnologyClasses();
                 },
-                touchstart: function(){
-                    removeTechnologyClasses();
-                    filterTechnologies(this);
-                },
             });
         },
-        filterTechnologies = function(el){
-            activeCategoryName = $(el).attr("class");
-            $(ui.list).addClass(activeCategoryName);
+        initTechnologiesSelect = function(){
+            cachedUi.$select.on("change", function(){
+                var val = $(this).val();
+                removeTechnologyClasses();
+                if (val !== "") {
+                    activeCategoryName = val;
+                    filterTechnologies(val);
+                }
+
+            });
+        },
+        filterTechnologies = function(activeCategoryName){
+            cachedUi.$list.find("li").each(function(){
+                if (!$(this).hasClass(activeCategoryName)) {
+                    $(this).addClass("animated-hide");
+                }
+            });
         },
         removeTechnologyClasses = function(){
-            $(ui.list).removeClass(activeCategoryName);
+            cachedUi.$list.find("li").removeClass("animated-hide");
             activeCategoryName = null;
         },
-        resetTechnologiesLegend = function(){
-            $(ui.el).on("click", function(e){
-                var $container = $(ui.legend);
-                if ($container.has(e.target).length === 0){
-                    removeTechnologyClasses();
-                }
+        decideSkillsImplementation = function(){
+            var viewport = site.views.run("common", "getViewportDimensions"),
+                isMobile = (viewport.height < cachedUi.$technologiesWrap.outerHeight()) || (Modernizr.touchevents && viewport.width < 640);
+
+            cachedUi.$technologiesWrap.toggleClass("mobile", isMobile);
+            cachedUi.$technologiesWrap.toggleClass("desktop", !isMobile);
+        },
+        cacheUi = function(){
+            for (var el in ui) {
+                cachedUi["$"+el] = $(ui[el]);
+            }
+            $(window).trigger("cachedUi");
+        },
+        onLoadResize = function(){
+            $(window).on("load."+viewName+" resize."+viewName, function(){
+                $(window).one("cachedUi", function(){
+                    decideSkillsImplementation();
+                });
+                cacheUi();
             });
         },
 
         exports = {
             init: function(){
-                shuffleTechnologies();
-                initTechnologiesLegend();
-                resetTechnologiesLegend();
+                onLoadResize();
+                $(window).one("cachedUi", function(){
+                    shuffleTechnologies();
+                    initTechnologiesLegend();
+                    initTechnologiesSelect();
+                });
             },
         };
 
