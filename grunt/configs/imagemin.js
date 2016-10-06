a00de2dd2814d9dc110e7dc3b1d7ec5768ebf83a
iamvanja <vanja@gavric.org>
2016-10-06 16:21:48 -0700

    Added image min optimizations that would make Speed Test not complain

--- grunt/configs/imagemin.js
+++ grunt/configs/imagemin.js
@@ -1,28 +1,48 @@
 "use strict";
+var mozjpeg = require("imagemin-mozjpeg");
+var extend = require("extend");
 
-module.exports = {
-    options: {
-        cache: false
+module.exports = function(grunt, data) {
+    var options = {
+        cache: false,
+        optimizationLevel: 6,
+        use: [mozjpeg({
+            quality: 90,
+        })]
     },
+    config = {
+        productionAll: {
+            options: {},
+            files:[
+                {
+                    expand: true,
+                    cwd: "build/assets/images/",
+                    src: [
+                        "**/*.{png,jpg,gif}",
+                        "!profile.jpg"
+                    ],
+                    dest: "build/assets/images/"
+                },
+                {
+                    expand: true,
+                    cwd: "build/v1/images/",
+                    src: [
+                        "**/*.{png,jpg,gif}"
+                    ],
+                    dest: "build/v1/images/"
+                },
+            ]
+        },
+        productionProfile: {
+            options: {},
+            files: {
+               "build/assets/images/profile.jpg": "build/assets/images/profile.jpg",
+            }
+        }
+    };
 
-    production: {
-        files:[
-            {
-                expand: true,
-                cwd: "build/assets/images/",
-                src: [
-                    "**/*.{png,jpg,gif}"
-                ],
-                dest: "build/assets/images/"
-            },
-            {
-                expand: true,
-                cwd: "build/v1/images/",
-                src: [
-                    "**/*.{png,jpg,gif}"
-                ],
-                dest: "build/v1/images/"
-            },
-        ]
-    }
+    extend(config.productionAll.options, options);
+    extend(config.productionProfile.options, options, {use: [mozjpeg({quality: 32})]});
+
+    return config;
 };
