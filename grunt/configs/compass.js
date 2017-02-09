aed16336c6d330bb967fe4710ab169a9bc9e5225
iamvanja <vanja@gavric.org>
2017-02-09 16:50:22 -0700

    Optimized config object for compass task

--- grunt/configs/compass.js
+++ grunt/configs/compass.js
@@ -1,36 +1,39 @@
 "use strict";
 
-module.exports = {
-    watch: {
-        options: {
-            // httpPath: "/",
-            cssDir: "src/assets/css",
-            sassDir: "src/assets/scss",
-            imagesDir: "src/assets/images",
-            javascriptsDir: "src/assets/js",
-            outputStyle: "expanded",
-            relativeAssets: true,
-            raw: "Sass::Script::Number.precision = 3\n",
-            importPath: "node_modules/foundation-sites/scss/",
-            require: [
-                "ceaser-easing"
-            ]
-        }
-    },
-    production: {
-        options: {
-            // httpPath: "/",
-            cssDir: "build/assets/css",
-            sassDir: "build/assets/scss",
-            imagesDir: "build/assets/images",
-            javascriptsDir: "build/assets/js",
-            outputStyle: "compressed",
-            relativeAssets: true,
-            raw: "Sass::Script::Number.precision = 3\n",
-            importPath: "node_modules/foundation-sites/scss/",
-            require: [
-                "ceaser-easing"
-            ]
-        }
-    }
+module.exports = function(grunt, data) {
+    var extend = require("extend"),
+        defaults = {
+            options: {
+                // httpPath: "/",
+                cssDir: "src/assets/css",
+                sassDir: "src/assets/scss",
+                imagesDir: "src/assets/images",
+                javascriptsDir: "src/assets/js",
+                outputStyle: "expanded",
+                relativeAssets: true,
+                raw: "Sass::Script::Number.precision = 3\n",
+                importPath: "node_modules/foundation-sites/scss/",
+                require: [
+                    "ceaser-easing"
+                ]
+            }
+        },
+        config = {
+            watch: {},
+            production: {}
+        };
+
+    extend(true, config.watch, defaults);
+    extend(true, config.production, defaults);
+    config.production.options.outputStyle = "compressed";
+
+    var propertiesToReplace = ["cssDir", "sassDir", "imagesDir", "javascriptsDir"];
+    propertiesToReplace.forEach(function(element) {
+        config.production.options[element] = config.production.options[element].replace("src/", "build/");
+    });
+
+    // console.log(config);
+
+    return config;
 };
+
