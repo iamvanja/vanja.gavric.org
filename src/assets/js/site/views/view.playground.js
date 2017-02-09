12145291ada062a7e497538b65ad64b05061f766
iamvanja <vanja@gavric.org>
2017-02-08 21:36:46 -0700

    Commented unused code from view.playground

--- src/assets/js/site/views/view.playground.js
+++ src/assets/js/site/views/view.playground.js
@@ -4,21 +4,9 @@
         viewName = "playground",
         ui = {
             el: ".playground",
-            // columns: ".playground-item .columns",
         },
-        // ensureEqualHeight = function(){
-        //     var $columns = $(ui.columns);
-        //     $(window).width() > 768 ? site.helpers.setAllToMaxHeight($columns) : $columns.css({"height":"auto"});
-        // },
-        // onLoadResize = function(){
-        //     // todo: throttle this event?
-        //     $(window).on("load resize", function(e){
-        //         ensureEqualHeight();
-        //     });
-        // },
         exports = {
             init: function(){
-                // onLoadResize();
             },
         };
 
